96224c4b7d160f596485366391624230d7c388c9
mataltw04 <20040518koko@gmail.com>
2023-04-03 12:01:13 +0900

    클래스와 마우스 인풋

---
+++ mata project/Assets/scripts/0403/Test_008.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player
+{   
+    private int hp = 100;                                       //hp를 선언한후 100의 값을 입력
+    private int Power = 50;                                     //power를 선언한 후 50의 값을 입력
+    public void Attack()
+    {
+        Debug.Log(this.Power + "데미지를 입었다.");            //this는 Player 클래스를 이야기한다.
+    }
+    public void Damage(int damage)                              //Damage 함수는 int 형태로 받은 데이미 인수를 받는다.
+    {
+        this.hp -= damage;
+        Debug.Log(damage + "데미지를 입었다.");
+    }
+
+    public int getHp()
+    {
+        return this.hp;
+    }
+}
+
+
+
+public class Test_008 : MonoBehaviour
+{
+    Player player_01 = new Player();                            //위에 만든 플레이어 클래스를 선언한다
+    Player player_02 = new Player();                            //위에 만든 플레이어 클래스를 선언한다
+    public Text player01HP;                                     //플레이어 HP를 보여주는 UI 선언
+    public Text player02HP;                                     //플레이어 HP를 보여주는 UI 선언
+
+    // Start is called before the first frame update
+    void Start()
+    {                   
+        player_01.Attack();                                     //생성한 player_01의 Attack 함수를 실행시킨다.
+        player_01.Damage(30);                                   //생성한 player_01의 Attack 함수를 실행시킨다.(인수 30을 넣어준다.)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player01HP.text = "Player 01 HP : " + player_01.getHp().ToString();
+        player02HP.text = "Player 02 HP : " + player_02.getHp().ToString();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            player_01.Damage(1);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            player_02.Damage(1);
+        }
+    }
+}
